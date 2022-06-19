ec17299895c67d256f0daa5e781491884b652719
ankitakhurana11 <44482771+ankitakhurana11@users.noreply.github.com>
2022-06-19 02:59:15 -0700

    Concise Python solution

--- 1122. Relative Sort Array (EM).py
+++ 1122. Relative Sort Array (EM).py
@@ -53,7 +53,20 @@ class Solution:
                     loop = loop +1
         return arr
         
+   
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        return sorted(arr1, key= (arr2 + sorted(arr1)).index)
+
+
+class Solution:
+    def relativeSortArray(self, A, B):
+        k = {b: i for i, b in enumerate(B)}
+        return sorted(A, key=lambda a: k.get(a, 1000 + a))
         
+          
+      
+     
         
         
         
